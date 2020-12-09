de6908a2d7635b3886c4080dbeda73be7e328988
DoctorSpace <40442366+DoctorSpace@users.noreply.github.com>
2020-12-09 09:10:00 +0300

    Add files via upload

---
+++ 1000.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <fstream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    ofstream file_out;
+    file_out.open("Data.txt");
+ 
+
+    for (int i = 0; i < 100; i++) {
+
+        file_out << i << ". Денис лох" << endl;
+
+    }
+}
